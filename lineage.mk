669bdcc4e74882e8774ca89ab8d204499f5e5e2d
rajeevkalra <rajeev@kalra.in>
2017-02-27 17:14:57 +0530

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,12 +1,12 @@
-$(call inherit-product, device/lge/d855/full_d855.mk)
+$(call inherit-product, device/lge/d858/full_d858.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_d855
+PRODUCT_NAME := lineage_d858
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_open_cis" \
     PRODUCT_NAME="g3_global_com" \
-    BUILD_FINGERPRINT="lge/g3_global_com/g3:6.0/MRA58K/15351124649f4:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_global_com-user 6.0 MRA58K 15351124649f4 release-keys"
+    BUILD_FINGERPRINT="lge/g3_open_cis/g3:$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_NUMBER):user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_open_cis-user $(PLATFORM_VERSION) $(BUILD_ID) $(BUILD_NUMBER) release-keys"
